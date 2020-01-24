9f3cc2afcbba28e0ffda4deef8eb7320b3438d59
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-01-24 08:36:21 -0700

    Agregar archivos de proyecto.

---
+++ PracticaKaraoke/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+using System.Windows.Threading;
+
+namespace PracticaKaraoke
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer; //intervalos de tiempo
+        //LeerArchivo
+        AudioFileReader reader;
+        //ComunicaciónConLaTarjetaDeAudio
+
+        //ExclusivoParaSalida
+        WaveOut output;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+        }
+
+        private void BtnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            pbCancion.Visibility = Visibility.Visible;
+
+
+        }
+    }
+}
